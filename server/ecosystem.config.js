8a74adcb32be100bf611a2eefed1ac771bc71e38
muhammad-jiku <muhammadjiku364@gmail.com>
2025-08-21 21:03:17 +0600

    ecosystem config file added

---
+++ server/ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: 'jikmunn-real-estate-enterprise',
+      script: 'yarn',
+      args: 'dev',
+      env: {
+        NODE_ENV: 'development',
+      },
+    },
+  ],
+};
